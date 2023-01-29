dca7087cc1c708e639b476e6c70a7e9278d4bbe9
yamoo9 <yamoo9@naver.com>
2023-01-29 12:32:29 +0900

    lecture. 변수와 상수
    - 변수 선언
    - 변수 초기화
    - 변수에 값 할당
    - 상수의 경우는 위 3가지를 모두 한 번에!!

--- practice/02-1-variables.js
+++ practice/02-1-variables.js
@@ -4,20 +4,31 @@
 
 /* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ------------ */
 
-// - 갯수 별 상품 가격 계산하기
-// - 구매 제품 가격의 총 합
-// - 1년 기준 일자 수
-// - 구매 결제 여부
-// - 구매 결제 내역
-// - 브랜드 접두사
-// - 오늘의 운세
-// - 상품 정보
-
-
 
 /* variables ----------------------------------------------------------- */
+// - 갯수 별 상품 가격 계산하기
+let calcProductPriceByQuantity;
 
+// - 구매 제품 가격의 총 합
+let sumTotalProductPrice;
+
+// - 구매 결제 여부
+let isPayPurchase = false;
+
+// - 구매 결제 내역
+let historyPayment;
+
+// - 오늘의 운세
+let todayHoroscope;
+
+// - 상품 정보
+let productInfo;
 
 
 /* constant variables -------------------------------------------------- */
+// - 1년 기준 일자 수
+const daysPerYear = 365;
+
+// - 브랜드 접두사
+const brandPrefix = 'EUID';
 
