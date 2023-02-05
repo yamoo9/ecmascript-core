154391d66a56980624d75a399ce421126fe39cae
yamoo9 <yamoo9@naver.com>
2023-02-05 13:15:56 +0900

    learn. jQuery 탭 메뉴 코드 리팩토링

--- practice-jquery/js/ui/tabs-y9.js
+++ practice-jquery/js/ui/tabs-y9.js
@@ -1,16 +1,36 @@
-const ACTIVE_CLASSNAME = 'active';
+// 코드 재사용 관점 : 사용자 정의 옵션
+const config = {
+  activeClassName: 'active',
+  tabSelector: 'button',
+  panelSelector: 'panel',
+};
 
+// Tabs 컴포넌트
 const $component = $('.tabs');
-const $tabs = $component.find('button');
-const $tabPanels = $component.find('article');
 
+// Tabs 내부에 제어할 컴포넌트 수집
+const $tabs = $component.find(config.tabSelector);
+const $tabPanels = $component.find(config.panelSelector);
+
+// 컴포넌트 초기화(initialization)
 function init() {
-  // 참고: https://mzl.la/3RxSNGJ
+  // 탭 버튼에 인덱스 설정
   setIndex();
+  // 이벤트 연결
   bindEvents();
 }
 
 function setIndex() {
+  // jQuery 방식
+  // $.each($tabs, (index, tab) => $(tab).attr('data-index', index));
+  // $tabs.each((index, tab) => $(tab).attr('data-index', index));
+
+  // 포인트!!
+  // 무조건 jQuery 코드를 사용하는 것은 좋지 않다.
+  // 필요한 부분에 한해서만큼은 DOM 스크립트를 활용!
+  // console.log($tabs); // [button, button, button]
+  // $tabs.each((index, tab) => tab.setAttribute('data-index', index));
+  // 참고: https://mzl.la/3RxSNGJ
   $tabs.each((index, tab) => (tab.dataset.index = index));
 }
 
@@ -19,19 +39,22 @@ function bindEvents() {
 }
 
 function handleActivePanel(e) {
+  // DOM Script
   if (e.target.matches('button')) {
-    let index = e.target.dataset.index;
+    let index = e.target.dataset.index; // <button data-index=""></button>
     removeActivatedTabPanel();
     activeTabPanel(index);
   }
 }
 
 function removeActivatedTabPanel() {
-  $tabPanels.filter('.active').removeClass(ACTIVE_CLASSNAME);
+  $tabPanels
+    .filter(`.${config.activeClassName}`)
+    .removeClass(config.activeClassName);
 }
 
 function activeTabPanel(index) {
-  $tabPanels.eq(index).addClass(ACTIVE_CLASSNAME);
+  $tabPanels.eq(index).addClass(config.activeClassName);
 }
 
 init();
