3bb0fe1a63fc0051a7758f9a4ee141ee19763c1f
yamoo9 <yamoo9@naver.com>
2023-02-19 14:40:31 +0900

    learn. 요소의 높이 비교 및 가장 큰 값 가져오는 방법

--- practice-debounce/demo.js
+++ practice-debounce/demo.js
@@ -14,3 +14,18 @@ function handleResize(e) {
 
 globalThis.addEventListener('scroll', debounce(handleScroll, 100));
 globalThis.addEventListener('resize', debounce(handleResize, 100));
+
+
+/* -------------------------------------------------------------------------- */
+
+const gnb = document.querySelector('.gnb');
+
+const oneDepthLists = Array.from(gnb.querySelectorAll('[data-deptch="1"]'));
+const oneDepthListsOfHeight = oneDepthLists.map(list => Math.ceil(list.getBoundingClientRect().height));
+
+// const maxHeightValue = oneDepthListsOfHeight.sort().reverse()[0];
+const maxHeightValue = oneDepthListsOfHeight.sort()[oneDepthListsOfHeight.length - 1];
+
+gnb.style.height = `${maxHeightValue}px`;
+console.log(maxHeightValue);
+gnb.style.backgroundColor = `#ff0`;
